731b4474eb5b290eee2fd53b052752f8d274e525
Erick <erickkan2004@gmail.com>
2024-11-28 18:32:49 -0300

    Adding style

--- introducao-js/calculator/script.js
+++ introducao-js/calculator/script.js
@@ -1,12 +1,11 @@
 window.onload = function(){
-    document.getElementById("Calcular").addEventListener("click", function(event) {
-        event.preventDefault()
-        num1 = document.getElementById("num1").value;
-        num2 = document.getElementById("num2").value;
+    document.querySelectorAll("#num1, #num2").forEach(e => {
+        e.addEventListener("focusout", function(){
+            num1 = document.getElementById("num1").value;
+            num2 = document.getElementById("num2").value;
 
-        document.getElementById("result1").value = parseInt(num1) + parseInt(num2);
-        
-
+            document.getElementById("result1").value = parseInt(num1) + parseInt(num2);
+        })
     })
 
     // document.getElementById("num4").addEventListener("focusout", function(event){
@@ -25,5 +24,27 @@ window.onload = function(){
             document.getElementById("result2").value = parseInt(num1) - parseInt(num2);
         })
     })
+
+    document.querySelectorAll("#num5, #num6").forEach(e => {
+        e.addEventListener("focusout", function(){
+            num1 = document.getElementById("num5").value;
+            num2 = document.getElementById("num6").value;
+
+            document.getElementById("result3").value = parseInt(num1) * parseInt(num2);
+        })
+    })
+
+    document.querySelectorAll("#num7, #num8").forEach(e => {
+        e.addEventListener("focusout", function(){
+            num1 = document.getElementById("num7").value;
+            num2 = document.getElementById("num8").value;
+
+            document.getElementById("result4").value = parseInt(num1) / parseInt(num2);
+        })
+    })
+
+    // document.getElementById("num1").addEventListener("click", function(e){
+    //     e.value = ""
+    // })
 
 }
