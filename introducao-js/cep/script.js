77877476b8360c53b790efa49d1272ad4d0b4ec0
Erick <erickkan2004@gmail.com>
2024-12-22 20:30:20 -0300

    viacep API

--- introducao-js/cep/script.js
+++ introducao-js/cep/script.js
@@ -13,7 +13,43 @@ function clearForm(){
     document.getElementById("estado").value=("");
 }
 
+function meu_callback(content){
+    if(!("erro" in content)){
+        document.getElementById("rua").value=(content.logradouro);
+        document.getElementById("bairro").value=(content.bairro);
+        document.getElementById("cidade").value=(content.localidade);
+        document.getElementById("estado").value=(content.uf);
+    } else{
+        clearForm();
+        alert("CEP não encontrado.")
+    }
+}
+
 function cepSearch(value){
-    alert(value);
+    let cep = value.replace(/\D/g, '');
+
+    if(cep != ""){
+        let cepValidate = /^[0-9]{8}$/;
+        if(cepValidate.test(cep)){
+            document.getElementById("rua").value="...";
+            document.getElementById("bairro").value="...";
+            document.getElementById("cidade").value="...";
+            document.getElementById("estado").value="...";
+
+            let script = document.createElement('script');
+            script.src = 'https://viacep.com.br/ws/' + cep + '/json/?callback=meu_callback';
+            
+            document.body.appendChild(script);
+            console.log(script)
+
+        } else{
+            clearForm();
+            alert("Formato do CEP inválido.");
+        }
+
+    } else{
+        clearForm();
+    }
+    
 
 }
